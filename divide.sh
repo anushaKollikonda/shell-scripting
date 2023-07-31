0a8a23703ba6f66d32ced6babad60502e1dd9330
anushakollikonda <kollikonda.anusha@gmail.com>
2023-07-31 16:25:00 +0530

    dividing-in-scripting

---
+++ divide.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#
+#
+#######################################
+#author : anusha 
+#version: 0,1
+#date : 31-07-2023
+#
+#
+#######################################################
+echo "enater a number"
+read a
+echo "enter second number to divide"
+read b
+c=$(($a/$b))
+echo "answer is $c"
