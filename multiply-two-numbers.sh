96793fdb315490b395f3c7d41de23486fb7866cb
anushakollikonda <kollikonda.anusha@gmail.com>
2023-07-31 16:14:21 +0530

    multiplication-two-number-in-scripting

---
+++ multiply-two-numbers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#
+#
+##################################################
+#author : anusha 
+#version: version0.1 
+#date :31-07-2023
+#task : multiplying-two-numbers
+#
+############################################################
+#
+#
+ echo "enter first numer"
+ read a
+ echo "eneter a second number"
+ read b
+ c=$(($a*$b))
+ echo "answer is $c"
