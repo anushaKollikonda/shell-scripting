b39f621d646c131e02dba397f39eca353da3af5a
anushakollikonda <kollikonda.anusha@gmail.com>
2023-07-31 16:05:18 +0530

    adding-two-numers in scripting

---
+++ add-two-numbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+#################################
+# author : anusha
+# version: 0.1
+# task : add two numbers through command line arguements  
+#
+# #####333333333333333333333333333333333333333333
+
+
+sum=$(($1+$2))
+echo "Sum is $sum" 
